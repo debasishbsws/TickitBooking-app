2b7d888dd47baa055be2447c8898e1efce02e7e2
debasishbsws <debasishbsws.abc@gmail.com>
2022-10-11 19:09:11 +0530

    cleaning code using functions

--- main.go
+++ main.go
@@ -4,22 +4,56 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+	const confrenceName = "GopherCon"
+	const maxAttendees = 50
+	var scanner = bufio.NewScanner(os.Stdin)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	confrenceName := "GopherCon"
-	const maxAttendees = 50
+	
 	var remainingSeats uint = maxAttendees
 
 	var bookings []string = make([]string, 0, maxAttendees);
 
 	for {
-		fmt.Printf("Wellcome to %s booking application.\n\n", confrenceName)
+		if !greetUsers(remainingSeats) {
+			break
+		}
+		
+		name, email, age, needSeats := getUsersInput()
+
+		isValidName, isValidEmail, isValidSeats := validateUserInputs(name, email, needSeats, remainingSeats)
+
+		if isValidName && isValidEmail && isValidSeats {
+			bookingMessage := bookTickits(name, email, age, needSeats, &remainingSeats, &bookings)
+			fmt.Printf("%v Thanks for registration\n\n",bookingMessage)
+		}else {
+			if !isValidSeats {
+				fmt.Printf("Invalid Tickits request. %d seats remaining.., please enter the appropreate number.\n\n", remainingSeats)
+			}
+			if !isValidName {
+				fmt.Println("Invalid Name!!!")
+			}
+			if !isValidEmail {
+				fmt.Println("Invalid Email!!!")
+			}
+			fmt.Print("Please try again...\n\n")
+		}
+		
+	}
+
+	fmt.Println(bookings)
+
+
+}
+
+func greetUsers(remainingSeats uint) bool {
+	fmt.Printf("Wellcome to %s booking application.\n\n", confrenceName)
 		if remainingSeats == 0 {
 			fmt.Printf("We are full!!, No seats available. Please come back next Year.\n")
-			break
+			return false
 		} else if remainingSeats < 5 {
 			fmt.Printf("Only %d seats left! Book Your tickits now.\n", remainingSeats)
 		} else {
@@ -27,13 +61,24 @@ func main() {
 		}
 		fmt.Println("To Book Your Seats..")
 
-		var (
+		return true
+}
+
+func validateUserInputs(name, email string, needSeats int, remainingSeats uint) (bool, bool, bool) {
+	isValidName := strings.Contains(name, " ")
+	isValidEmail := strings.Contains(email, "@") && strings.Contains(email, ".")
+	isValidSeats := needSeats >= 0 && (uint(needSeats) <= remainingSeats)
+	return isValidName , isValidEmail, isValidSeats
+}
+
+func getUsersInput()(string, string, uint,int) {
+	var (
 			name 	string
 			email 	string
 			age uint
-			needSeats uint
+			needSeats int
 		)
-		fmt.Print("\nEnter Your Name : ")
+		fmt.Print("\nEnter Your Full Name : ")
 		scanner.Scan()
 		name = scanner.Text()
 
@@ -46,20 +91,12 @@ func main() {
 
 		fmt.Print("\nEnter How many seats you need : ")
 		fmt.Scan(&needSeats)
+	return name, email, age, needSeats
+}
 
-		if needSeats > remainingSeats {
-			fmt.Println("Sorry!! We can't book your tickits, Only", remainingSeats, "seats remain")
-			fmt.Print("Please try again...\n\n")
-			continue
-		} else {
-			bookingmessage := fmt.Sprintf("Your booking info\n\tConference Name: %s,\n\tTickit holder Name: %s,Age: %d,\n\tEmail: %s,\n\tTicket: %d\n", confrenceName, name, age, email, needSeats)
-			remainingSeats -= needSeats
-			bookings = append(bookings, bookingmessage)
-			fmt.Printf("%v Thanks for registration\n\n",bookingmessage)
-		}
-	}
-
-	fmt.Println(bookings)
-
-
+func bookTickits(name string, email string, age uint, needSeats int, remainingSeats *uint, bookings *[]string) string {
+	bookingmessage := fmt.Sprintf("Your booking info\n\tConference Name: %s,\n\tTickit holder Name: %s,Age: %d,\n\tEmail: %s,\n\tTicket: %d\n", confrenceName, name, age, email, needSeats)
+	*remainingSeats -= uint(needSeats)
+	*bookings = append(*bookings, bookingmessage)
+	return bookingmessage
 }
